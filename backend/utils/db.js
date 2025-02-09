220fdf2c92521821cece3e79d82440e7701cc51c
RahulSidar08 <rahulsidar2056@gmail.com>
2025-02-09 23:34:04 +0530

    fix

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -4,6 +4,7 @@ dotenv.config();
 const connectDB = async () => {
     try {
         await mongoose.connect(`${process.env.MONGO_URI}`);
+        console.log("mongo url is correct")
         console.log('mongodb connected successfully');
     } catch (error) {
         console.log(error);
