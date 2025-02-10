5dda0c15726bbb68f35144ef964185cd39af5fce
RahulSidar08 <rahulsidar2056@gmail.com>
2025-02-10 23:54:00 +0530

    fixed api issue

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-console.log("MongoDB URI:", process.env.MONGO_URL); 
+
 const connectDB = async () => {
     try {
        await mongoose.connect(`${process.env.MONGO_URL}`);
-        console.log("mongo url is correct")
         console.log('mongodb connected successfully');
     } catch (error) {
         console.log(error);
